a28c5f3b690556318e72b3c83f6d0dd8bd0d7fab
naide05 <naide05@gmail.com>
2018-05-25 10:43:46 +0200

    action bar con icono de añadir

--- app/src/main/java/proyectofinal/dam/joaquin/listacompra/MainActivity.kt
+++ app/src/main/java/proyectofinal/dam/joaquin/listacompra/MainActivity.kt
@@ -2,11 +2,38 @@ package proyectofinal.dam.joaquin.listacompra
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import proyectofinal.dam.joaquin.listacompra.R.id.main__btn_action__new
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            main__btn_action__new -> {
+                //TODO - crear producto nuevo en un nuevo activity o fragment y añadirlo a la lista
+                main__label__demo.text = getString(R.string.demo_text)
+
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+
+
     }
 }
